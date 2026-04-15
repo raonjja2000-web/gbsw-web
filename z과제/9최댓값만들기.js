e7f1d2c82c8d09d926d5e9fdfcf22d0de799c289
raonjja2000-web <raonjja2000@gmail.com>
2026-04-15 15:09:04 +0900

    Update 9최댓값만들기.js

--- z과제/9최댓값만들기.js
+++ z과제/9최댓값만들기.js
@@ -1,22 +1,24 @@
 function solution(numbers) {
     var answer=0
-    //numbers.sort((a, b) => b - a);
-    let max=0
-
-    for(let i=0;i<numbers.length;i++){
-        if (numbers[i]>numbers[max]){
-            max=i
-        }
-    }
-    let maxI = numbers[maxIndex]
-    numbers[maxIndex]=0
-
-    maxIndex=0;
-    for(let i=0;i<numbers.length;i++){
-        if (numbers[i]>numbers[maxIndex]){
-            maxIndex=i
-        }
-    }
-    return numbers;
+    numbers.sort((a, b) => b - a);
+    return numbers[0]*numbers[1]
 }
-console.log(solution([0, 31, 24, 10, 1, 9]))
+    
+//  let max=0
+
+//     for(let i=0;i<numbers.length;i++){
+//         if (numbers[i]>numbers[maxIndex]){
+//             maxIndex=i
+//         }
+//     }
+//     numbers[maxIndex]=0
+
+//     maxIndex=0;
+//     for(let i=0;i<numbers.length;i++){
+//         if (numbers[i]>numbers[maxIndex]){
+//             maxIndex=i
+//         }
+//     }
+//     return numbers;
+// }
+// console.log(solution([0, 31, 24, 10, 1, 9]))
